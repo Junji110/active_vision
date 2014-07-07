1e05084ac370799c1d0ff7298395c3c8155e2556
Junji Ito <j.ito@fz-juelich.de>
2014-07-07 07:24:33 +0200

    changed default parameter values in prep/lvd2hdf5.py

--- prep/lvd2hdf5.py
+++ prep/lvd2hdf5.py
@@ -7,9 +7,11 @@ import h5py
 import active_vision.fileio.lvdread as lvdread
 import active_vision.utils as avutils
 
-def load_data_in_chunks(reader, dsfactor=10, chunksize=500000, verbose=False):
+def load_data_in_chunks(reader, dsfactor=10, chunksize=5000000, verbose=False):
     param = reader.get_param()
     data_length = param['data_length']
+    if chunksize >= data_length:
+        chunksize = (data_length / dsfactor) * dsfactor
     num_chunk = data_length / chunksize
 
     data = np.empty((num_ch, data_length / dsfactor))
@@ -37,7 +39,7 @@ if __name__ == "__main__":
     parser.add_argument("--hdf5dir", default=projectdir)
     parser.add_argument("--sbj", default="HIME")
     parser.add_argument("--data", nargs=2, default=(20140529, 6))
-    parser.add_argument("--dsfactor", type=int, default=100)
+    parser.add_argument("--dsfactor", type=int, default=10)
     arg = parser.parse_args()
     
     sbj = arg.sbj
@@ -72,10 +74,11 @@ if __name__ == "__main__":
         wideband = load_data_in_chunks(reader, dsfactor=dsfactor, chunksize=5000000, verbose=True)
 
         # save data in hdf5
-        fn = "{0}/{1}_rec{2}_pc{3}.hdf5".format(arg.hdf5dir, sess, rec, i_pc+1)
-        with h5py.File(fn, 'w') as f:
+        fn_hdf5 = "{0}/{1}_rec{2}_pc{3}.hdf5".format(arg.hdf5dir, sess, rec, i_pc+1)
+        with h5py.File(fn_hdf5, 'w') as f:
             f.create_dataset("wideband", data=wideband)
             f.create_dataset("Fs", data=Fs)
             f.create_dataset("Fs_orig", data=Fs_orig)
             f.create_dataset("file", data=fn_pc)
             f.create_dataset("downsample_factor", data=dsfactor)
+        print 'PC{0} data file "{1}" converted to "{2}"'.format(i_pc + 1, fn_pc, fn_hdf5)
