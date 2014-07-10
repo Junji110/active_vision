4293c3ec8bd193c7a6f4cb7df764454487164b39
Junji Ito <j.ito@fz-juelich.de>
2014-07-10 16:49:55 +0200

    refactored find_filenames() using the regular expression for filename matching

--- utils.py
+++ utils.py
@@ -1,33 +1,31 @@
 import os
+import re
 
 import scipy.io as spio
 
 def find_filenames(datadir, subject, session, rec, filetype):
     if filetype not in ['imginfo', 'stimtiming', 'param', 'parameter', 'task', 'daq', 'lvd', 'odml', 'hdf5']:
-        raise ValueError("filetype must be either of 'imginfo', 'stimtiming', 'param', 'parameter', 'task', or 'daq'.")
+        raise ValueError("Filetype {0} is not supported.".format(filetype))
     
-    # identify the names of metadata files
-    if filetype in ['daq', 'lvd', 'odml', 'hdf5']:
+    if filetype in ['daq', 'lvd', 'hdf5', 'odml']:
         searchdir = "{dir}/{sbj}/{sess}".format(dir=datadir, sbj=subject, sess=session)
-        if filetype in ['daq', 'lvd']:
-            searchtoken = 'rec{0}_pc'.format(rec)
-        else:
-            searchtoken = filetype
+        re_filename = re.compile('{sess}.*_rec{rec}.*\.{filetype}$'.format(sess=session, rec=rec, filetype=filetype))
     else:
         searchdir = "{dir}/{sbj}/{sess}/{sess}_rec{rec}".format(dir=datadir, sbj=subject, sess=session, rec=rec)
-        searchtoken = filetype
+        re_filename = re.compile(".*{0}.*".format(filetype))
         
     filenames = os.listdir(searchdir)
     fn_found = []
     for fn in filenames:
-        if searchtoken in fn:
+        match = re_filename.match(fn)
+        if match:
             fn_found.append("{0}/{1}".format(searchdir, fn))
+
     if len(fn_found) == 0:
         raise IOError("Files of type '{0}' not found.".format(filetype))
+    else:
+        return fn_found
     
-    return fn_found
-
-
 def get_imgID(stimdir, stimsetname):
     print stimsetname
     for i in range(1, 49):
@@ -38,5 +36,6 @@ def get_imgID(stimdir, stimsetname):
 
 if __name__ == "__main__":
     stimdir = "C:/Users/ito/datasets/osaka/stimuli"
-    stimsetname = "fv_random_4_large_stripes"
+#     stimsetname = "fv_random_3"
+    stimsetname = "fv_random_5_large_stripes"
     get_imgID(stimdir, stimsetname)
