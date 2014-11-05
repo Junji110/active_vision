a6fafb1ba9483841c9fba110d265c25b0bd0f1cf
Junji Ito <j.ito@fz-juelich.de>
2014-11-05 16:08:07 +0100

    The program converts a single data file rather than pc1,2,3 files at once

--- prep/lvd2hdf5.py
+++ prep/lvd2hdf5.py
@@ -1,8 +1,9 @@
-from argparse import ArgumentParser
+import sys
+import argparse
 
 import h5py
 
-# import active_vision
+sys.path.append("/users/ito/toolbox")
 import active_vision.fileio.lvdread as lvdread
 import active_vision.utils as avutils
 
@@ -12,54 +13,53 @@ if __name__ == "__main__":
     from active_vision.conf.conf_files import projectdir, rawdir
     
     # parse command line options
-    parser = ArgumentParser()
+    parser = argparse.ArgumentParser()
     parser.add_argument("--lvddir", default=rawdir)
     parser.add_argument("--hdf5dir", default=projectdir)
     parser.add_argument("--sbj", default="HIME")
-    parser.add_argument("--data", nargs=2, default=(20140528, 1))
+    parser.add_argument("--data", nargs=3, default=(20140528, 1, 'pc1'))
     parser.add_argument("--dsfactor", type=int, default=10)
     arg = parser.parse_args()
     
     sbj = arg.sbj
-    sess, rec = arg.data
+    sess, rec, pc = arg.data
     dsfactor = arg.dsfactor
 
     # --- find filenames
-    fn_lvd = ['', '', '']
-    fn_lvd_found = avutils.find_filenames(arg.lvddir, sbj, sess, rec, 'lvd')
-    for i_pc in range(3):
-        fn = [x for x in fn_lvd_found if 'pc{0}'.format(i_pc+1) in x]
-        if len(fn) > 0:
-            fn_lvd[i_pc] = fn[0]
-    
-    for i_pc, fn_pc in enumerate(fn_lvd):
-        if fn_pc is '':
-            print "PC{0} data file not found.".format(i_pc + 1)
-            continue
-        
-        # initialize LVD Reader
-        reader = lvdread.LVDReader(fn_pc)
-        
-        # load parameters
-        header = reader.get_header()
-        param = reader.get_param()
-        
-        fn_hdf5 = "{0}/{1}_rec{2}_pc{3}.hdf5".format(arg.hdf5dir, sess, rec, i_pc+1)
-        with h5py.File(fn_hdf5, 'w') as f:
-            grp = f.create_group("/param")
-            for key in param:
-                grp.create_dataset(key, data=param[key])
-            grp.create_dataset("downsample_factor", data=dsfactor)
+    fn_lvd = avutils.find_filenames(arg.lvddir, sbj, sess, rec, 'lvd')
+    if len(fn_lvd) == 0:
+        raise IOError("LVD data file not found in {0}".format(arg.lvddir))
 
-            grp = f.create_group("/header")
-            for key in header:
-                grp.create_dataset(key, data=header[key])
+    for fn in fn_lvd:
+        if pc in fn:
+            fn_pc = fn
+            break
+    else:
+        raise IOError("{0} data file not found.".format(pc))
 
-            grp = f.create_group("/data")
-            for chname in header['AIUsedChannelName']:
-                print "Processing channel {0}...".format(chname)
-                data = reader.get_data(channel=chname)
-                grp.create_dataset(chname, data=data[0, ::dsfactor])
-                print "Channel {0} done.".format(chname)
+    # initialize LVD Reader
+    reader = lvdread.LVDReader(fn_pc)
 
-        print 'PC{0} data file "{1}" converted to "{2}"'.format(i_pc + 1, fn_pc, fn_hdf5)
+    # load parameters
+    header = reader.get_header()
+    param = reader.get_param()
+
+    fn_hdf5 = "{0}/{1}_rec{2}_{3}.hdf5".format(arg.hdf5dir, sess, rec, pc)
+    with h5py.File(fn_hdf5, 'w') as f:
+        grp = f.create_group("/param")
+        for key in param:
+            grp.create_dataset(key, data=param[key])
+        grp.create_dataset("downsample_factor", data=dsfactor)
+
+        grp = f.create_group("/header")
+        for key in header:
+            grp.create_dataset(key, data=header[key])
+
+        grp = f.create_group("/data")
+        for chname in header['AIUsedChannelName']:
+            print "Processing channel {0}...".format(chname)
+            data = reader.get_data(channel=chname)
+            grp.create_dataset(chname, data=data[0, ::dsfactor])
+            print "Channel {0} done.".format(chname)
+
+    print 'LVD data file "{0}" converted to "{1}"'.format(fn_pc, fn_hdf5)
