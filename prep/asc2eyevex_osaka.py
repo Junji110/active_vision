c407f6e1036bab7a878d8095d1508fbb27b8f474
Junji Ito <j.ito@fz-juelich.de>
2016-06-12 00:40:09 +0200

    Fix parameter import of asc2***_osaka.py

--- prep/asc2eyevex_osaka.py
+++ prep/asc2eyevex_osaka.py
@@ -6,13 +6,14 @@ import numpy as np
 sys.path.append("/users/ito/toolbox/cili")
 from cili.util import load_eyelink_dataset
 
-from parameters.asc2eyevex import *
+from parameters.asc2eyevex_osaka import *
 
 fields = ("TIMING_CLOCK", "g_task_switch", "g_rec_no", "g_block_num", "TRIAL_NUM", "log_task_ctrl", "t_tgt_data", "SF_FLG")
 
 for sbj, sess, rec, blk, tasktype, taskID in dataset_info:
     fn_data = "{dir}/{task}/{sbj}/{sbj}{rec}00{blk}{sym}.asc".format(
         dir=datadir, task=tasktype, sbj=sbj, rec=rec, blk=blk, sym=tasktype[0])
+    print "Load data from {}".format(fn_data)
     samps, events = load_eyelink_dataset(fn_data)
     saccades = events.dframes['ESACC']
     fixations = events.dframes['EFIX']
@@ -96,3 +97,4 @@ for sbj, sess, rec, blk, tasktype, taskID in dataset_info:
         dir=prepdir, sbj=sbj, rec=rec, blk=blk, sym=tasktype[0])
     with open(fn_eyevex, "w") as f:
         f.write("\n".join(eyevex_lines))
+        print "Data saved in {}".format(fn_eyevex)
--- prep/asc2task_osaka.py
+++ prep/asc2task_osaka.py
@@ -6,13 +6,14 @@ import numpy as np
 sys.path.append("/users/ito/toolbox/cili")
 from cili.util import load_eyelink_dataset
 
-from parameters.asc2task import *
+from parameters.asc2task_osaka import *
 
 fields = ("TIMING_CLOCK", "g_task_switch", "g_rec_no", "g_block_num", "TRIAL_NUM", "log_task_ctrl", "t_tgt_data", "SF_FLG")
 
 for sbj, sess, rec, blk, tasktype, taskID in dataset_info:
     fn_data = "{dir}/{task}/{sbj}/{sbj}{rec}00{blk}{sym}.asc".format(
         dir=datadir, task=tasktype, sbj=sbj, rec=rec, blk=blk, sym=tasktype[0])
+    print "Load data from {}".format(fn_data)
     samps, events = load_eyelink_dataset(fn_data)
     msgs = events.dframes['MSG']
 
@@ -31,6 +32,7 @@ for sbj, sess, rec, blk, tasktype, taskID in dataset_info:
     # pick indices of trial starts and ends
     idxs_trial_on = np.where(msgs.label.values == "TRIALID")[0]
     idxs_trial_off = np.where(msgs.label.values == "TRIAL_END")[0]
+    print len(idxs_trial_on), len(idxs_trial_off)
     assert(len(idxs_trial_on) == len(idxs_trial_off))
 
 
@@ -64,7 +66,6 @@ for sbj, sess, rec, blk, tasktype, taskID in dataset_info:
                 clkcnt_img_on = msgs.index.values[i]
                 x_img_on = samps[samps.index < clkcnt_img_on].x_l.values[-1]
                 y_img_on = samps[samps.index < clkcnt_img_on].y_l.values[-1]
-                print np.hypot(x_img_on, y_img_on)
                 if np.isnan(x_img_on) or np.isnan(y_img_on) or np.hypot(x_img_on, y_img_on) >= 1.0:
                     sf_flg = 0
                 output = map(str, [msgs.index.values[i], taskID, rec, blk, trialID, 311, stimID, sf_flg])
@@ -74,7 +75,8 @@ for sbj, sess, rec, blk, tasktype, taskID in dataset_info:
                 continue
             task_lines.append(",".join(output))
 
-    fn_task = "{dir}/{sbj}/{sbj}{rec}00{blk}{sym}_task.csv".format(
+    fn_task = "{dir}/{task}/{sbj}/{sbj}{rec}00{blk}{sym}_task.csv".format(
         dir=datadir, task=tasktype, sbj=sbj, rec=rec, blk=blk, sym=tasktype[0])
     with open(fn_task, "w") as f:
         f.write("\n".join(task_lines))
+        print "Data saved in {}".format(fn_task)
