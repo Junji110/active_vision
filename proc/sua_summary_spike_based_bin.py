8bcdcdb55a4be58ea6269aea8caf294294b96058
Junji Ito <j.ito@fz-juelich.de>
2016-11-04 15:16:33 +0100

    Improve the appearance of plots by sua_summary_spike_based_bin.py

--- proc/sua_summary_spike_based_bin.py
+++ proc/sua_summary_spike_based_bin.py
@@ -57,17 +57,15 @@ def gap(data, refs=None, nrefs=10, ks=range(1, 11)):
 
 if __name__ == "__main__":
     # file information
-    # datasetdir = "z:"
-    # datasetdir = "/users/junji/desktop/ito/datasets/osaka"
     datasetdir = "/home/ito/datasets/osaka"
     rawdir = "{}/RAWDATA".format(datasetdir)
     prepdir = "{}/PREPROCESSED".format(datasetdir)
-    savedir = "."
+    savedir = "./scratch"
 
     # analysis parameters
     sampling_rate = 20000.0
-    bin_size = 500  # bin width in number of spikes
-    bin_step = 50  # bin step in number of spikes
+    bin_size = 200  # bin width in number of spikes
+    bin_step = 20  # bin step in number of spikes
     num_bin_hist = 40
 
     # plot parameters
@@ -77,36 +75,42 @@ if __name__ == "__main__":
     cluster_type = ""
     # cluster_type = "Demerged"
 
-    estimate_nums_clst = True
-    # estimate_nums_clst = False
+    detect_change_point = True
+    # detect_change_point = False
+
+    # estimate_nums_clst = True
+    estimate_nums_clst = False
 
     savefig = True
     # savefig = False
 
     # session information
     datasets = [
-        # ["HIME", "20140908", 4, "pc1", "09081309V1hp2"],
-        # ["HIME", "20140908", 4, "pc1", "09081319V1hp2"],
-        # ["HIME", "20140908", 4, "pc2", "09081319IThp2"],
-        # ["SATSUKI", "20150811", 6, "pc1", "08111157rec6V1hp2"],
-        # ["SATSUKI", "20150811", 6, "pc2", "08111157rec6IThp2"],
-        ["SATSUKI", "20151027", 5, 2, "IT"],
-        ["SATSUKI", "20151027", 5, 2, "V1"],
-        ["SATSUKI", "20151110", 7, 2, "IT"],
-        ["SATSUKI", "20151110", 7, 2, "V1"],
-        ]
+        # ["HIME", "20140908", 4, 0, "09081319V1hp2"],
+        # ["HIME", "20140908", 4, 0, "09081319IThp2"],
+        # ["SATSUKI", "20150811", 6, 0, "08111157rec6V1hp2"],
+        # ["SATSUKI", "20150811", 6, 0, "08111157rec6IThp2"],
+        ["HIME", "20140908", 4, 11, "V1"],
+        # ["HIME", "20140908", 4, 11, "IT"],
+        # ["SATSUKI", "20151027", 5, 2, "IT"],
+        # ["SATSUKI", "20151027", 5, 2, "V1"],
+        # ["SATSUKI", "20151110", 7, 2, "IT"],
+        # ["SATSUKI", "20151110", 7, 2, "V1"],
+    ]
+
 
     for dataset in datasets:
         sbj, sess, rec, blk, site = dataset
-        fn_spikes = "{}_rec{}_blk{}_{}_h".format(sess, rec, blk, site)
-        print "\n{sbj}:{fn_spikes}".format(**locals())
 
         # set filenames
-        # fn_class = "{dir}/{sbj}/spikes/24ch_unselected/{fn}.class_{typ}Cluster".format(dir=prepdir, sbj=sbj, fn=fn_spikes, typ=cluster_type)
-        fn_class = "{dir}/tmp/new/{fn}.class_{typ}Cluster".format(dir=prepdir, sbj=sbj, fn=fn_spikes, typ=cluster_type)
-        # fn_seltypes = "{dir}/{sbj}/spikes/24ch/{fn}.types_SelectedCluster".format(dir=prepdir, sbj=sbj, fn=fn_spikes)
-        fn_seltypes = "{dir}/tmp/new/{fn}.types_SelectedCluster".format(dir=prepdir, sbj=sbj, fn=fn_spikes)
         fn_task = utils.find_filenames(rawdir, sbj, sess, rec, 'task')[0]
+        if blk == 0:
+            fn_spikes = site
+            fn_class = "{dir}/{sbj}/spikes/24ch_unselected/{fn}.class_{typ}Cluster".format(dir=prepdir, sbj=sbj, fn=fn_spikes, typ=cluster_type)
+        else:
+            fn_spikes = "{}_rec{}_blk{}_{}_h".format(sess, rec, blk, site)
+            fn_class = "{dir}/tmp/new/{fn}.class_{typ}Cluster".format(dir=prepdir, sbj=sbj, fn=fn_spikes, typ=cluster_type)
+        print "\n{sbj}:{fn_spikes}".format(**locals())
 
         # load task events
         print "\tLoading task data file..."
@@ -115,31 +119,26 @@ if __name__ == "__main__":
         ts_blk_on = []
         ts_blk_off = []
         tasks_blk = []
-        for blk in blks:
-            blk_events, blk_param = utils.load_task(fn_task, blk)
+        for b in blks:
+            blk_events, blk_param = utils.load_task(fn_task, b)
             tasks_blk.append(blk_param['task'][0])
             ts_blk_on.append(blk_events["evtime"][0] / sampling_rate)
             ts_blk_off.append(blk_events["evtime"][-2] / sampling_rate)
+        recdur = ts_blk_off[-1]
         print "\t...done.\n"
-
-        # get the list of selected units
-        dataset = np.genfromtxt(fn_seltypes, dtype=None, names=True)
-        selunitID = np.unique(dataset["type"])
 
         # load data
         print "\tLoading spike data file..."
         dataset = np.genfromtxt(fn_class, skip_header=2, dtype=None, names=True)
+        dataset["event_time"] += ts_blk_on[blk]
         num_ch = len(dataset.dtype.names) - 3
-        recdur = dataset["event_time"][-1]
         print "\t...done.\n"
 
         unitID = np.unique(dataset["type"])
         for i_unit, uid in enumerate(unitID):
-            selected = True if uid in selunitID else False
-
             mask_unit = dataset["type"] == uid
             num_spike = mask_unit.sum()
-            if num_spike <= 2*bin_size:
+            if num_spike <= bin_size:
                 print "\tUnit {} has too few spikes ({} spikes in {} sec).\n".format(uid, num_spike, recdur)
                 continue
 
@@ -157,46 +156,50 @@ if __name__ == "__main__":
             unit_ch = spike_covs.mean(1).argmax()
 
             # compute time resolved measures
-            bin_edges = np.arange(0, num_spike - 2 * bin_size, bin_step)
+            bin_edges = np.arange(0, num_spike - bin_size, bin_step)
             num_bin = bin_edges.size
             bin_times = np.empty(num_bin)
-            bin_times_pval = np.empty(num_bin)
-            covs = np.zeros((num_ch, num_bin))
-            cov_stds = np.zeros((num_ch, num_bin))
-            cov_pvals = np.zeros(num_bin)
-            isis = np.zeros(num_bin)
-            isi_stds = np.zeros(num_bin)
-            isi_pvals = np.zeros(num_bin)
+            cov_means = np.zeros((num_ch, num_bin))
+            isi_means = np.zeros(num_bin)
             spike_size_hist = np.zeros((num_bin_hist, num_bin))
-            nums_clst = np.zeros(num_bin)
+            if estimate_nums_clst:
+                nums_clst = np.zeros(num_bin)
+            if detect_change_point:
+                bin_times_pval = np.empty(num_bin)
+                cov_pvals = np.zeros(num_bin)
+                isi_pvals = np.zeros(num_bin)
             for i, idx_ini in enumerate(bin_edges):
-                idxs_spikes_in_bin1 = np.arange(idx_ini, idx_ini + bin_size)
-                idxs_spikes_in_bin2 = np.arange(idx_ini + bin_size, idx_ini + 2 * bin_size)
-
-                bin_times[i] = (spike_times[idxs_spikes_in_bin1[0]] + spike_times[idxs_spikes_in_bin1[-1]]) / 2
-                bin_times_pval[i] = (spike_times[idxs_spikes_in_bin1[0]] + spike_times[idxs_spikes_in_bin2[-1]]) / 2
+                idxs_spikes_in_bin = np.arange(idx_ini, idx_ini + bin_size)
+                bin_times[i] = spike_times[idxs_spikes_in_bin][[0, -1]].mean()
 
                 # inter-spike interval
-                isi1 = np.diff(spike_times[idxs_spikes_in_bin1])
-                isi2 = np.diff(spike_times[idxs_spikes_in_bin2])
-                _, isi_pvals[i] = spstats.ks_2samp(isi1, isi2)
-                isis[i] = isi1.mean()
-                isi_stds[i] = isi1.std()
+                isi = np.diff(spike_times[idxs_spikes_in_bin])
+                isi_means[i] = isi.mean()
 
                 # covariance as spike size
-                cov1 = spike_covs[:, idxs_spikes_in_bin1]
-                cov2 = spike_covs[:, idxs_spikes_in_bin2]
-                _, cov_pvals[i] = spstats.ks_2samp(cov1[unit_ch], cov2[unit_ch])
-                covs[:, i] = cov1.mean(1)
-                cov_stds[:, i] = cov1.std(1)
+                cov = spike_covs[:, idxs_spikes_in_bin]
+                cov_means[:, i] = cov.mean(1)
+
+                if detect_change_point:
+                    if idx_ini + 2*bin_size < num_spike:
+                        idxs_spikes_in_bin2 = np.arange(idx_ini + bin_size, idx_ini + 2*bin_size)
+                        bin_times_pval[i] = (spike_times[idxs_spikes_in_bin[0]] + spike_times[idxs_spikes_in_bin2[-1]]) / 2
+                        isi2 = np.diff(spike_times[idxs_spikes_in_bin2])
+                        _, isi_pvals[i] = spstats.ks_2samp(isi, isi2)
+                        cov2 = spike_covs[:, idxs_spikes_in_bin2]
+                        _, cov_pvals[i] = spstats.ks_2samp(cov[unit_ch], cov2[unit_ch])
 
                 # spike size histogram
-                hist, bin_edges_hist = np.histogram(cov1[unit_ch], range=[0, 200], bins=num_bin_hist)
+                hist, bin_edges_hist = np.histogram(cov[unit_ch], range=[0, 200], bins=num_bin_hist)
                 spike_size_hist[:, i] = hist
+
                 if estimate_nums_clst:
-                    gaps = gap(cov1[unit_ch][:, np.newaxis], nrefs=1, ks=[1, 2])
+                    gaps = gap(cov[unit_ch][:, np.newaxis], nrefs=1, ks=[1, 2])
                     nums_clst[i] = gaps.argmax() + 1
-                    print "\t{} in {}".format(i, len(bin_edges))
+
+                if i % 100 == 99:
+                    print "\t...{} of {} bins processed...".format(i+1, len(bin_edges))
+
 
             print "\t...done.\n"
 
@@ -204,8 +207,6 @@ if __name__ == "__main__":
             plt.figure(figsize=(10, 8))
             plt.subplots_adjust(left=0.08, right=0.96)
             title = "{} unit {} (Ch {}, {} spikes)".format(fn_spikes, uid, unit_ch, num_spike)
-            if selected:
-                title +="*"
             title += "\nbin size: {} spks, bin step: {} spks".format(bin_size, bin_step)
             plt.suptitle(title)
             gs = gridspec.GridSpec(5, 2, width_ratios=[4, 1])
@@ -217,8 +218,8 @@ if __name__ == "__main__":
                 bin_times,
                 np.linspace(-0.5, num_ch-0.5, num_ch+1)
             )
-            vmax = np.abs(covs).max()
-            plt.pcolormesh(X, Y, covs, vmax=vmax, vmin=-vmax, cmap="bwr")
+            vmax = np.abs(cov_means).max()
+            plt.pcolormesh(X, Y, cov_means, vmax=vmax, vmin=-vmax, cmap="bwr")
             plt.xlim(0, recdur)
             plt.ylim(23, 0)
             plt.grid(color="gray")
@@ -228,8 +229,8 @@ if __name__ == "__main__":
             plt.xlabel("Time (s)")
             plt.ylabel("Spike size (cov)")
             plt.plot(spike_times, spike_covs[unit_ch], ",", color="black")
-            for i_blk, blk in enumerate(blks):
-                if blk == 0:
+            for i_blk, b in enumerate(blks):
+                if b == 0:
                     continue
                 plt.axvspan(ts_blk_on[i_blk], ts_blk_off[i_blk], color=colors_task[tasks_blk[i_blk]], alpha=0.1)
             plt.xlim(0, recdur)
@@ -263,29 +264,32 @@ if __name__ == "__main__":
             ax1 = plt.subplot(gs[6])
             plt.xlabel("Time (s)")
             plt.ylabel("Spike size (cov)")
-            plt.plot(bin_times, covs[unit_ch], color="black")
-            # plt.fill_between(bin_times, covs[unit_ch]-2*cov_stds[unit_ch], covs[unit_ch]+2*cov_stds[unit_ch], color="black", linewidth=0, alpha=0.2)
+            plt.plot(bin_times, cov_means[unit_ch], color="black")
+            plt.xlim(0, recdur)
             plt.ylim(ymin=0)
             plt.grid(color="gray")
-            ax2 = ax1.twinx()
-            plt.ylabel("Surprise")
-            plt.plot(bin_times_pval, np.log10((1.0 - cov_pvals) / cov_pvals), color="magenta")
-            plt.axhline(y=0, color="magenta", linestyle=":")
-            plt.xlim(0, recdur)
-            plt.ylim(-10, 10)
+            if detect_change_point:
+                ax2 = ax1.twinx()
+                plt.ylabel("Surprise")
+                plt.plot(bin_times_pval, np.log10((1.0 - cov_pvals) / cov_pvals), color="magenta")
+                plt.axhline(y=0, color="magenta", linestyle=":")
+                plt.xlim(0, recdur)
+                plt.ylim(-10, 10)
 
             ax1 = plt.subplot(gs[8])
             plt.xlabel("Time (s)")
             plt.ylabel("Firing rate (1/s)")
-            plt.plot(bin_times, 1.0/isis, color="black")
+            plt.plot(bin_times, 1.0 / isi_means, color="black")
+            plt.xlim(0, recdur)
             plt.ylim(ymin=0)
             plt.grid(color="gray")
-            ax2 = ax1.twinx()
-            plt.ylabel("Surprise")
-            plt.plot(bin_times_pval, np.log10((1.0 - isi_pvals) / isi_pvals), color="magenta")
-            plt.axhline(y=0, color="magenta", linestyle=":")
-            plt.xlim(0, recdur)
-            plt.ylim(-10, 10)
+            if detect_change_point:
+                ax2 = ax1.twinx()
+                plt.ylabel("Surprise")
+                plt.plot(bin_times_pval, np.log10((1.0 - isi_pvals) / isi_pvals), color="magenta")
+                plt.axhline(y=0, color="magenta", linestyle=":")
+                plt.xlim(0, recdur)
+                plt.ylim(-10, 10)
 
             if savefig:
                 fn_fig = "{}/{}_unit{}.png".format(savedir, fn_spikes, uid)
