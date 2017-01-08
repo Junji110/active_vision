b02e58e248a4a06a0acd4da17ded4a8e39d5f309
Junji Ito <j.ito@fz-juelich.de>
2017-01-09 00:59:07 +0100

    Refactor lvd2hdf5.py to load parameters from a parameter file

--- prep/lvd2hdf5.py
+++ prep/lvd2hdf5.py
@@ -8,71 +8,62 @@ sys.path.append("/users/ito/toolbox")
 import active_vision.fileio.lvdread as lvdread
 import active_vision.utils as avutils
 
+from parameters.lvd2hdf5 import *
+
 
 if __name__ == "__main__":
-    # --- load parameters from configuration files
-    from active_vision.conf.conf_files import projectdir, rawdir
-    
     # parse command line options
     parser = argparse.ArgumentParser()
-    parser.add_argument("--lvddir", default=rawdir)
-    parser.add_argument("--hdf5dir", default=projectdir)
-    parser.add_argument("--sbj", default="HIME")
-    parser.add_argument("--data", nargs=3, default=(20140528, 1, 'pc1'))
-    parser.add_argument("--dsfactor", type=int, default=10)
-    parser.add_argument("--chunk_size", type=int, default=6000000)
+    parser.add_argument("--lvddir", default=datadir_default)
+    parser.add_argument("--hdf5dir", default=savedir_default)
+    parser.add_argument("--dsfactor", type=int, default=dsfactor_default)
+    parser.add_argument("--chunk_size", type=int, default=chunk_size_default)
     arg = parser.parse_args()
-    
-    sbj = arg.sbj
-    sess, rec, pc = arg.data
     dsfactor = arg.dsfactor
     chunk_size = arg.chunk_size
+    datadir = arg.lvddir
+    savedir = arg.hdf5dir
 
-    # --- find filenames
-    fn_lvd = avutils.find_filenames(arg.lvddir, sbj, sess, rec, 'lvd')
-    if len(fn_lvd) == 0:
-        raise IOError("LVD data file not found in {0}".format(arg.lvddir))
+    for sbj, sess, rec, pc in datasets:
+        dataset_name = "{sbj}:{sess}_rec{rec}".format(**locals())
+        print "\n{dataset_name} being processed...".format(**locals())
 
-    for fn in fn_lvd:
-        if pc in fn:
-            fn_pc = fn
-            break
-    else:
-        raise IOError("{0} data file not found.".format(pc))
+        # --- find filenames
+        fn_lvd = avutils.find_filenames(arg.lvddir, sbj, sess, rec, 'lvd', pc)[0]
 
-    # initialize LVD Reader
-    reader = lvdread.LVDReader(fn_pc)
+        # initialize LVD Reader
+        reader = lvdread.LVDReader(fn_lvd)
 
-    # load parameters
-    header = reader.get_header()
-    param = reader.get_param()
-    data_length = param['data_length']
-    num_chunk = data_length / chunk_size
-    chunk_len = chunk_size / dsfactor
+        # load parameters
+        header = reader.get_header()
+        param = reader.get_param()
+        data_length = param['data_length']
+        num_chunk = data_length / chunk_size
+        chunk_size_ds = chunk_size / dsfactor
 
-    fn_hdf5 = "{0}/{1}_rec{2}_{3}.hdf5".format(arg.hdf5dir, sess, rec, pc)
-    with h5py.File(fn_hdf5, 'w') as f:
-        grp = f.create_group("/param")
-        for key in param:
-            grp.create_dataset(key, data=param[key])
-        grp.create_dataset("downsample_factor", data=dsfactor)
+        fn_hdf5 = "{savedir}/{sess}_rec{rec}_{pc}.hdf5".format(**locals())
+        with h5py.File(fn_hdf5, 'w') as f:
+            grp = f.create_group("/param")
+            for key in param:
+                grp.create_dataset(key, data=param[key])
+            grp.create_dataset("downsample_factor", data=dsfactor)
 
-        grp = f.create_group("/header")
-        for key in header:
-            grp.create_dataset(key, data=header[key])
+            grp = f.create_group("/header")
+            for key in header:
+                grp.create_dataset(key, data=header[key])
 
-        grp = f.create_group("/data")
-        for chname in header['AIUsedChannelName']:
-            print "Processing channel {0}...".format(chname)
-            chdata = np.empty(data_length / dsfactor)
-            for i_chunk in range(num_chunk):
-                data_chunk = reader.get_data(channel=chname, samplerange=(i_chunk * chunk_size, (i_chunk + 1) * chunk_size))
-                chdata[i_chunk * chunk_len:(i_chunk + 1) * chunk_len] = data_chunk[0][::10]
-                print "\tdata chunk {0} of {1} loaded.".format(i_chunk, num_chunk)
-            data_chunk = reader.get_data(channel=chname, samplerange=(num_chunk * chunk_size, data_length))
-            chdata[num_chunk * chunk_len:] = data_chunk[0][::10]
+            grp = f.create_group("/data")
+            for chname in header['AIUsedChannelName']:
+                print "\t...{chname} being loaded...".format(**locals())
+                chdata = np.empty(data_length / dsfactor)
+                for i_chunk in range(num_chunk):
+                    data_chunk = reader.get_data(channel=chname, samplerange=(i_chunk * chunk_size, (i_chunk+1) * chunk_size))
+                    chdata[i_chunk * chunk_size_ds : (i_chunk + 1) * chunk_size_ds] = data_chunk[0][::dsfactor]
+                    print "\t\t...data chunk {0} of {1} loaded...".format(i_chunk+1, num_chunk)
+                data_chunk = reader.get_data(channel=chname, samplerange=(num_chunk * chunk_size, data_length))
+                chdata[num_chunk * chunk_size_ds:] = data_chunk[0][::dsfactor]
 
-            grp.create_dataset(chname, data=chdata)
-            print "Channel {0} done.".format(chname)
+                grp.create_dataset(chname, data=chdata)
+                print "\t...done.".format(**locals())
 
-    print 'LVD data file "{0}" converted to "{1}"'.format(fn_pc, fn_hdf5)
+        print '...all done. LVD data file "{fn_lvd}" converted to "{fn_hdf5}"'.format(**locals())
