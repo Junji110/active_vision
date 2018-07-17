9955a1b8fec6192c0d7e304388bc79b97d1ba7b7
Junji Ito <j.ito@fz-juelich.de>
2018-07-17 18:27:29 +0200

    Add functions for artifact saccade removal to utils.py

--- utils.py
+++ utils.py
@@ -400,6 +400,144 @@ def extract_fixsac_pair(eye_events, mode="fixsac"):
     return eye_events[idx_all]
 
 
+def identify_artifact_saccades(eye_events, coeffs):
+    # idntify artifact saccade based on amp-velo-accl relations
+    amp = np.hypot(eye_events['x_off']-eye_events['x_on'], eye_events['y_off']-eye_events['y_on'])
+    velo = np.array(eye_events['param1'])
+    accl = np.array(eye_events['param2'])
+
+    mask_sac = eye_events['eventID']==100
+    mask_amp2velo = velo > coeffs['amp2velo'] * amp
+    mask_amp2accl = accl > coeffs['amp2accl'] * amp
+    mask_velo2accl = accl > coeffs['velo2accl'] * velo
+
+    # return np.where(mask_sac & mask_amp2accl & mask_velo2accl)[0]
+    return np.where(mask_sac & mask_amp2velo & mask_amp2accl & mask_velo2accl)[0]
+
+
+def remove_artifact_saccades(eye_events, idx_artsac, fixdur_min=0.04, sampling_rate=20000):
+    def truncate_fixations(fix_pre, fix_post):
+        dur_pre = fix_pre['off'] - fix_pre['on']
+        dur_post = fix_post['off'] - fix_post['on']
+        fix_trunc = {'eventID': 200}
+        for key in fix_pre:
+            if key in ('param1', 'param2'):
+                fix_trunc[key] = (dur_pre*fix_pre[key] + dur_post*fix_post[key]) / (dur_pre + dur_post)
+            elif key in ('off', 'x_off', 'y_off'):
+                fix_trunc[key] = fix_post[key]
+            elif key in ('on', 'x_on', 'y_on'):
+                fix_trunc[key] = fix_pre[key]
+        return fix_trunc
+
+    def reconstruct_fixation_from_inter_saccade_interval(sac_pre, sac_post):
+        fix_recon = {
+            'eventID': 200,
+            'on': sac_pre['off'],
+            'off': sac_post['on'],
+            'x_on': sac_pre['x_off'],
+            'y_on': sac_pre['y_off'],
+            'x_off': sac_post['x_on'],
+            'y_off': sac_post['y_on'],
+            'param1': (sac_pre['x_off'] + sac_post['x_on']) / 2,
+            'param2': (sac_pre['y_off'] + sac_post['y_on']) / 2,
+        }
+        return fix_recon
+
+    # convert the input array to a dict of lists, so that deletion of elements in the middle of sequence can be done
+    # easily with list.pop()
+    events = {key: eye_events[key].tolist() for key in eye_events.dtype.names}
+
+    # as the fixation-truncation algorithm doesn't work for an artifact saccade at the very end of the eye event array,
+    # this needs to be removed before applying the algorithm.
+    while idx_artsac[-1] == len(events['on']) - 1:
+        i = idx_artsac[-1]
+        for key in events:
+            events[key].pop(i)
+        if events['eventID'][i-1] == 200:
+            # Since any fixation must be preceded and followed by proper saccades, if the removed artifact saccade is
+            # following a fixation, we need to remove this fixation too.
+            for key in events:
+                events[key].pop(i-1)
+        idx_artsac = idx_artsac[:-1]
+
+    # Scan through artifact saccades and truncate the fixations splitted by the artifact saccade.
+    for i in idx_artsac[::-1]:
+        # The scan is backwards from the last artifact saccade, so that the modification of the event list (removal of
+        # the scanned artifact saccade and truncation of the surrounding fixations) doesn't change the positions of the
+        # rest of artifact saccades in the list.
+
+        eventID_pre = events['eventID'][i-1]
+        eventID_post = events['eventID'][i+1]
+        if (eventID_pre == 200) and (eventID_post == 200):
+            # Case where the both preceding and following events are fixation:
+            #   just truncate these fixations
+            fix_pre = {key: events[key][i-1] for key in events}
+            fix_post = {key: events[key][i+1] for key in events}
+            fix_trunc = truncate_fixations(fix_pre, fix_post)
+            for key in events:
+                events[key].pop(i+1)
+                events[key].pop(i)
+                events[key][i-1] = fix_trunc[key]
+        elif eventID_post == 200:
+            # Case where only the following event is a fixation:
+            #   check if the gap to the preceding saccade is shorter than fixdur_min. If it is, it is likely to be cut
+            #   out from the following fixation by the current artifact saccade, so truncate the gap to the following
+            #   fixation.
+            gap = events['on'][i] - events['off'][i-1]
+            if gap < fixdur_min * sampling_rate:
+                sac_pre = {key: events[key][i-1] for key in events}
+                sac_post = {key: events[key][i] for key in events}
+                fix_pre = reconstruct_fixation_from_inter_saccade_interval(sac_pre, sac_post)
+                fix_post = {key: events[key][i+1] for key in events}
+                fix_trunc = truncate_fixations(fix_pre, fix_post)
+                for key in events:
+                    events[key][i+1] = fix_trunc[key]
+                    events[key].pop(i)
+            else:
+                for key in events:
+                    events[key].pop(i+1)
+                    events[key].pop(i)
+        elif eventID_pre == 200:
+            # Case where only the preceding event is a fixation:
+            #   check if the gap to the following saccade is shorter than fixdur_min. If it is, truncate it to the
+            #   preceding fixation.
+            gap = events['on'][i+1] - events['off'][i]
+            if gap < fixdur_min * sampling_rate:
+                sac_pre = {key: events[key][i] for key in events}
+                sac_post = {key: events[key][i+1] for key in events}
+                fix_pre = {key: events[key][i-1] for key in events}
+                fix_post = reconstruct_fixation_from_inter_saccade_interval(sac_pre, sac_post)
+                fix_trunc = truncate_fixations(fix_pre, fix_post)
+                for key in events:
+                    events[key].pop(i)
+                    events[key][i-1] = fix_trunc[key]
+            else:
+                for key in events:
+                    events[key].pop(i)
+                    events[key].pop(i-1)
+
+        else:
+            # Case where the artifact saccade is not followed and not preceded by a fixation:
+            #   just remove the artifact saccade
+            for key in events:
+                events[key].pop(i)
+
+    if events['eventID'][0] == 200:
+        # This case can happen when the very first event in the input eye event array was an artifact saccade. Since
+        # any fixation must be preceded and followed by proper saccades, we remove this fixation which is the very
+        # first event in the result of the artifact saccade removal.
+        for key in events:
+            events[key].pop(0)
+
+    eye_events_cleaned = np.empty_like(eye_events)
+    eye_events_cleaned = eye_events_cleaned[:len(events['on'])]
+    for key in events:
+        eye_events_cleaned[key] = events[key]
+
+    return eye_events_cleaned
+
+
+
 if __name__ == "__main__":
     stimdir = "C:/Users/ito/datasets/osaka/stimuli"
     stimsetnames = (
